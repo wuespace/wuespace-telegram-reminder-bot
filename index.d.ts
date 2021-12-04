fde0d9ff22d63b52ee72849f61c09248b8688714
Pablo Klaschka <contact@pabloklaschka.de>
2021-12-04 22:36:58 +0100

    docs: Document the `Reminder` interface

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,18 @@
+/**
+ * A reminder definition that can get added in the `reminders.js` file.
+ */
 declare interface Reminder {
+	/**
+	 * The CRON expression that determines the schedule at which the
+	 * {@link message} gets sent.
+	 *
+	 * @see https://crontab.guru/ CronTab (a website to quickly configure CRON
+	 * expressions))
+	 */
 	cron: string;
+	/**
+	 * The message that gets sent to the channel on the schedule defined by
+	 * {@link cron}.
+	 */
 	message: string;
 }
